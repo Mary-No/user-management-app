bf998fab082f4c2dfef17071c70c5547c4e5d755
Mary-No <mascha.novik@gmail.com>
2025-05-30 11:40:25 +0200

    add front link

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: /^http:\/\/localhost:\d+$/,
+    origin: [/^http:\/\/localhost:\d+$/, 'https://mary-no.github.io/'],
     credentials: true,
 }));
 
